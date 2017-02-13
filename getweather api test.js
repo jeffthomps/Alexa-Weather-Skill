3049b87a2f2fd4388477ff250057f89ab8f353bb
Jeff <jeffthomps@comcast.net>
2017-02-12 21:52:03 -0500

    Update getweather api test.js

--- getweather api test.js
+++ getweather api test.js
@@ -1,10 +1,12 @@
 //This is test code to understand how to get and parse json API
 //data from a website 
+// api.wunderground.com/api/e7320a5cc75e4a51/conditions/q/pws:KMICOMST2.json
+
 
 var https = require('https'); var options = {
-  host: 'callook.info',
+  host: 'api.wunderground.com',
   port: 443,
-  path: '/kd8psj/json',
+  path: '/api/e7320a5cc75e4a51/conditions/q/pws:KMICOMST2.json',
   method: 'GET'
 };
 
@@ -21,11 +23,13 @@ var req = https.get(options, function(res) {
 			try {
 				var getResponse = JSON.parse(body);
 				//console.log("Response body: ", getResponse);
-				console.log("status: ", getResponse.status);
-				console.log("current.operClass: ", getResponse.current.operClass);
-				console.log("name: ", getResponse.name);
-				console.log("address line 2: ", getResponse.address.line2);
-				console.log("otherInfo.expiryDate: ", getResponse.otherInfo.expiryDate);
+				console.log("temp_f", getResponse.current_observation.temp_f);
+				console.log("wind_mph", getResponse.current_observation.wind_mph);
+				//console.log("current.operClass: ", getResponse.current.operClass);
+
+				//console.log("name: ", getResponse.name);
+				//console.log("address line 2: ", getResponse.address.line2);
+				//console.log("otherInfo.expiryDate: ", getResponse.otherInfo.expiryDate);
 				}
 			catch (e) {
 					console.log('Error parsing JSON!');
