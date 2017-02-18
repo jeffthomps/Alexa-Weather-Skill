879bd5b68a8e0264d65a93bd9f755b1fefb0c594
Jeff <jeffthomps@comcast.net>
2017-02-18 14:14:27 -0500

    Create Intent Schema.js

---
+++ Intent Schema.js
@@ -0,0 +1,17 @@
+{
+  "intents": [
+    {
+      "intent": "conditions",
+      "slots": [
+        {
+          "name": "temperature",
+          "type": "AMAZON.LITERAL"
+        },
+        {
+          "name": "windSpeed",
+          "type": "AMAZON.LITERAL"
+        }
+      ]
+    }
+  ]
+}
