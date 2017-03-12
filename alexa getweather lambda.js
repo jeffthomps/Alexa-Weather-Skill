0156946ecc312342a76a2015bc076b5fc222c2b7
Jeff <jeffthomps@comcast.net>
2017-03-12 12:36:51 -0400

    Update alexa getweather lambda.js

--- alexa getweather lambda.js
+++ alexa getweather lambda.js
@@ -41,7 +41,7 @@ exports.handler = (event, context) => {
         console.log(`INTENT REQUEST`)
 
 /*
-		var endpoint = "https://api.wunderground.com/api/"+process.env.wundergroundApiKey+"/conditions/q/pws:KMICOMST2.json"
+			var endpoint = "https://api.wunderground.com/api/e7320a5cc75e4a51/conditions/q/pws:KMICOMST2.json"
             var body = ""
             https.get(endpoint, (response) => {
               response.on('data', (chunk) => { body += chunk })
@@ -68,6 +68,7 @@ exports.handler = (event, context) => {
         switch(event.request.intent.name) {
           case "conditions":
             var endpoint = "https://api.wunderground.com/api/"+process.env.wundergroundApiKey+"/conditions/q/pws:KMICOMST2.json"
+            //var endpoint = "https://api.wunderground.com/api/"+process.env.wundergroundApiKey+"/conditions/q/pws:KMIGRAND34.json"
             var body = ""
             //console.log("testvariable= "+$testVariable); 
             https.get(endpoint, (response) => {
@@ -77,9 +78,12 @@ exports.handler = (event, context) => {
 				var temp_f = data.current_observation.temp_f
 				var wind_mph = data.current_observation.wind_mph
 				var wind_dir = data.current_observation.wind_dir
+				var wind_dir_speech = windDirConvert (wind_dir);
+				var wind_gust_mph = data.current_observation.wind_gust_mph ;
                 context.succeed(
                   generateResponse(
-                    buildSpeechletResponse(`Current temperature is ${temp_f} degrees, wind speed is ${wind_mph} miles per hour from the ${wind_dir}.`, true),
+                    buildSpeechletResponse(`Current temperature is ${temp_f} degrees, wind speed is ${wind_mph} miles per hour from the ${wind_dir_speech}, gusting to ${wind_gust_mph}.`, true),
+                    //buildSpeechletResponse(`Current temperature is ${temp_f} degrees, wind speed is ${wind_mph} miles per hour from the ${wind_dir}.`, true),
                     {}
                   )
                 //console.log("testvariable= "+$testVariable);  
@@ -144,6 +148,68 @@ exports.handler = (event, context) => {
   } catch(error) { context.fail(`Exception: ${error}`) }
 
 }
+
+
+//Convert wind direction abbreviation to words
+function windDirConvert (wind_dir) {
+	var windDirConverted = "" 
+	//console.log("IN FUNCTION wind_dir= "+wind_dir);
+	switch (wind_dir) {
+		case "East":
+			windDirConverted = "east";
+			break;
+		case "ENE":
+			windDirConverted = "east northeast";
+			break;
+		case "ESE":
+			windDirConverted = "east southeast";
+			break;
+		case "NE":
+			windDirConverted = "northeast";
+			break;
+		case "NNE":
+			windDirConverted = "north northeast";
+			break;
+		case "NNW":
+			windDirConverted = "north northwest";
+			break;
+		case "North":
+			windDirConverted = "north";
+			break;
+		case "NW":
+			windDirConverted = "northwest";
+			break;
+		case "SE":
+			windDirConverted = "southeast";
+			break;
+		case "South":
+			windDirConverted = "south";
+			break;
+		case "SSE":
+			windDirConverted = "south southeast";
+			break;
+		case "SSW":
+			windDirConverted = "south southwest";
+			break;
+		case "SW":
+			windDirConverted = "southwest";
+			break;
+		case "Variable":
+			windDirConverted = "variable";
+			break;
+		case "West":
+			windDirConverted = "west";
+			break;
+		case "WNW":
+			windDirConverted = "west northwest";
+			break;
+		case "WSW":
+			windDirConverted = "west southwest";
+		}
+	return windDirConverted;
+	}
+
+
 
 // Helpers
 buildSpeechletResponse = (outputText, shouldEndSession) => {
