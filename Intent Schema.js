18149c4c15344ff2519883f8c44ebb68e61a54ed
Jeff <jeffthomps@comcast.net>
2017-03-11 20:22:09 -0500

    Update Intent Schema.js

--- Intent Schema.js
+++ Intent Schema.js
@@ -1,17 +1,13 @@
 {
   "intents": [
     {
-      "intent": "conditions",
-      "slots": [
-        {
-          "name": "temperature",
-          "type": "AMAZON.LITERAL"
-        },
-        {
-          "name": "windSpeed",
-          "type": "AMAZON.LITERAL"
-        }
-      ]
+      "intent": "conditions" 
+    },
+	    {
+      "intent": "temperature" 
+    },
+	    {
+      "intent": "windSpeed" 
     }
   ]
 }
